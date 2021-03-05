bcae90bde247e038a049d445e1b8f26b2a581e23
YuryGusarov <77166688+YuryGusarov@users.noreply.github.com>
2021-03-05 11:56:41 +0300

    Update SPET.py

--- SPET.py
+++ SPET.py
@@ -7,7 +7,17 @@
 print("Enter your Human Genome SNP")
 Input_DNA = input()
 print(type(Input_DNA))
-#Hello World!
+logpas = {
+    "uname": 'def', 
+    "upasswd": 'def'
+}
+login = input('Enter email: ')
+passwd = input('Password: ')
+logpas['uname'] = login
+logpas['password'] = passwd
+url = 'https://www.ncbi.nlm.nih.gov/account/?back_url=https%3A%2F%2Fwww.ncbi.nlm.nih.gov%2F'
+s = requests.Session()
+loging = s.post(url, data = logpas)
 
 # In[ ]:
 
